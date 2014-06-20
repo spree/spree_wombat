6f9ce6fdbecc0bf20614bf4330de7caaa05689a0
Peter Berkenbosch <peter@pero-ict.nl>
2014-06-20 14:34:53 +0200

    remove binding.pry

--- lib/spree/wombat/handler/add_shipment_handler.rb
+++ lib/spree/wombat/handler/add_shipment_handler.rb
@@ -80,7 +80,6 @@ module Spree
           shipment_attributes = shipment.slice *Spree::Shipment.attribute_names
           shipment_attributes["inventory_units_attributes"] = inventory_units_attributes
           shipment_attributes["address_attributes"] = address_attributes
-          binding.pry
           shipment = Spree::Shipment.create!(shipment_attributes)
           shipment.shipping_methods << shipping_method
           shipment.refresh_rates
