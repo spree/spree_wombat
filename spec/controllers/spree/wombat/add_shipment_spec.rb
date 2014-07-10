386a57a9b4c182e5d379a1adf988f63124a62908
Peter Berkenbosch <peter@pero-ict.nl>
2014-07-10 15:38:45 +0200

    add spec for unknown state on address and country not requiring states

--- spec/controllers/spree/wombat/add_shipment_spec.rb
+++ spec/controllers/spree/wombat/add_shipment_spec.rb
@@ -5,40 +5,39 @@ module Spree
 
     let!(:message) {
       {
-  "shipment"=> {
-    "id"=> "356195",
-    "order_id"=> "R063443587",
-    "cost"=> "12.0",
-    "status"=> "shipped",
-    "shipping_method"=> "UPS Ground",
-    "tracking"=> "1ZA282180433242244246",
-    "shipped_at"=> "2014-06-08T22=>00=>00-07=>00",
-    "shipping_address"=> {
-      "firstname"=> "John",
-      "lastname"=> "Do",
-      "address1"=> "199 Awesome Ave",
-      "address2"=> "",
-      "zipcode"=> "123456",
-      "city"=> "Awesometown",
-      "state"=> "PA",
-      "country"=> "US",
-      "phone"=> "3424253345434"
-    },
-    "items"=> [
-      {
-        "name"=> "Oremus-StarterPack",
-        "product_id"=> "Oremus-StarterPack",
-        "quantity"=> 1
-      },
-      {
-        "name"=> "Oremus-3DVD",
-        "product_id"=> "Oremus-3DVD",
-        "quantity"=> 2
+        "shipment"=> {
+          "id"=> "356195",
+          "order_id"=> "R063443587",
+          "cost"=> "12.0",
+          "status"=> "shipped",
+          "shipping_method"=> "UPS Ground",
+          "tracking"=> "1ZA282180433242244246",
+          "shipped_at"=> "2014-06-08T22=>00=>00-07=>00",
+          "shipping_address"=> {
+            "firstname"=> "John",
+            "lastname"=> "Do",
+            "address1"=> "199 Awesome Ave",
+            "address2"=> "",
+            "zipcode"=> "123456",
+            "city"=> "Awesometown",
+            "state"=> "PA",
+            "country"=> "US",
+            "phone"=> "3424253345434"
+          },
+          "items"=> [
+            {
+              "name"=> "Oremus-StarterPack",
+              "product_id"=> "Oremus-StarterPack",
+              "quantity"=> 1
+            },
+            {
+              "name"=> "Oremus-3DVD",
+              "product_id"=> "Oremus-3DVD",
+              "quantity"=> 2
+            }
+          ]
+        }
       }
-    ]
-  }
-}
-
     }
 
     let(:parameters) {
@@ -56,7 +55,6 @@ module Spree
 
     let!(:shipping_method) { create(:shipping_method, name: 'UPS Ground (USD)')}
     let!(:country) { Spree::Country.first }
-    let!(:state) { create(:state, :country => country, name: "Pennsylvania", abbr: "PA") }
 
     before do
       Spree::Variant.stub(:find_by_sku).and_return(order.variants.first)
@@ -73,9 +71,26 @@ module Spree
         end
 
         context 'and an existing handler for the webhook' do
+
+          let!(:state) { create(:state, :country => country, name: "Pennsylvania", abbr: "PA") }
+
           it 'will process the webhook handler' do
             post 'consume', parameters
             json = JSON.parse(response.body)
+            expect(response).to be_success
+          end
+        end
+
+        context 'with unknown address state_name and country does not require a state' do
+
+          before do
+            Spree::Country.any_instance.stub(:states_required) { false }
+          end
+
+          it 'will succesfully process the webhook handler' do
+            post 'consume', parameters
+            json = JSON.parse(response.body)
+            binding.pry
             expect(response).to be_success
           end
         end
