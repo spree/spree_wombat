4bd6cae943a178e49ddedd50a6cc60a828d8e8f0
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-22 14:43:57 -0400

    Example setup should default to filtering complete orders.

--- lib/generators/spree_wombat/install/templates/wombat.rb
+++ lib/generators/spree_wombat/install/templates/wombat.rb
@@ -5,8 +5,9 @@ Spree::Wombat::Config.configure do |config|
 
   # config.push_objects = ["Spree::Order", "Spree::Product"]
   # config.payload_builder = {
-  #   "Spree::Order" => {:serializer => "Spree::Wombat::OrderSerializer", :root => "orders"},
-  #   "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products"},
+  #   # By default we filter orders to only push if they are completed.  You can remove the filter to send incomplete orders as well.
+  #   "Spree::Order" => { serializer: "Spree::Wombat::OrderSerializer", root: "orders", filter: "complete" },
+  #   "Spree::Product" => { serializer: "Spree::Wombat::ProductSerializer", root: "products" },
   # }
   #config.push_url = "https://push.wombat.co"
 
