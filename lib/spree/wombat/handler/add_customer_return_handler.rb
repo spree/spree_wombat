6652952dcfe495ffad75c0723ff15d02ef6051be
Andrew Thal <athal7@me.com>
2014-08-05 21:10:08 -0400

    Use sort_by to correctly sort return items in the customer return handler

--- lib/spree/wombat/handler/add_customer_return_handler.rb
+++ lib/spree/wombat/handler/add_customer_return_handler.rb
@@ -52,9 +52,9 @@ module Spree
         end
 
         def sort_return_items(return_items)
-          return_items = return_items.sort { |ri| -(ri.created_at || DateTime.now).to_i }
-          return_items = return_items.sort { |ri| ri.return_authorization.try(:number) == customer_return_params[:rma] ? 0 : 1 }
-          return_items.sort { |ri| ri.persisted? ? 0 : 1 }
+          return_items = return_items.sort_by { |ri| -(ri.created_at || DateTime.now).to_i }
+          return_items = return_items.sort_by { |ri| ri.return_authorization.try(:number) == customer_return_params[:rma] ? 0 : 1 }
+          return_items.sort_by { |ri| ri.persisted? ? 0 : 1 }
         end
 
         def reimburse_customer_return!(customer_return)
