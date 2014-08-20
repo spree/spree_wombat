92e8ca56312990ab7a36cfb9eff497313111dd26
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-20 16:31:27 -0400

    Build image so its not created until save!  is called.

--- lib/spree/wombat/handler/product_handler_base.rb
+++ lib/spree/wombat/handler/product_handler_base.rb
@@ -102,7 +102,7 @@ module Spree
           return unless images.present?
 
           images.each do |image_hsh|
-            image = variant.images.create
+            image = variant.images.build
             image.attachment = open(URI.parse(URI.encode(image_hsh["url"].strip)))
             image.position = image_hsh["position"]
             image.alt = image_hsh["title"]
