6e0d28237e2b81e13d16fd52628cc4c2f086b714
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-22 12:48:01 -0400

    Source isnt always credit card. Fixes #26
    Conflicts:
    	spec/serializers/spree/wombat/customer_return_serializer_spec.rb
    	spec/serializers/spree/wombat/inventory_unit_serializer_spec.rb
    	spec/serializers/spree/wombat/refund_serializer_spec.rb
    	spec/serializers/spree/wombat/reimbursement_serializer_spec.rb
    	spec/serializers/spree/wombat/return_item_serializer_spec.rb
    
    Conflicts:
    	app/serializers/spree/wombat/source_serializer.rb

---
+++ app/serializers/spree/wombat/source_serializer.rb
@@ -0,0 +1,25 @@
+require 'active_model/serializer'
+
+module Spree
+  module Wombat
+    class SourceSerializer < ActiveModel::Serializer
+      attributes :name, :cc_type, :last_digits, :source_type
+
+      def name
+        object.name
+      end
+
+      def cc_type
+        object.try(:cc_type) || "N/A"
+      end
+
+      def last_digits
+        object.try(:last_digits) || "N/A"
+      end
+
+      def source_type
+        object.class.to_s
+      end
+    end
+  end
+end
