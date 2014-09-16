c6c1bb05bb244c9e1efb44ff5158b7f3a5de4153
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-09-16 16:49:22 +0200

    Add specs to find stock location by name or admin name

--- spec/lib/spree/wombat/handler/set_inventory_handler_spec.rb
+++ spec/lib/spree/wombat/handler/set_inventory_handler_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
+shared_examples "updates inventory level and responds with proper message" do
+  let!(:stock_item) do
+    item = variant.stock_items.first
+    item.stock_location = stock_location
+    item.save
+    item
+  end
+
+  it "will set the inventory to the supplied amount" do
+    expect{handler.process}.to change{stock_item.reload.count_on_hand}.from(0).to(93)
+  end
+
+  it "returns a Hub::Responder with a proper message" do
+    responder = handler.process
+    expect(responder.summary).to eql "Set inventory for SPREE-T-SHIRT at us_warehouse from 0 to 93"
+    expect(responder.code).to eql 200
+  end
+end
+
 module Spree
   module Wombat
     describe Handler::SetInventoryHandler do
@@ -9,23 +28,18 @@ module Spree
       describe "process" do
 
         context "with stock item present" do
-          let!(:stock_location) { create(:stock_location, name: 'us_warehouse')}
-          let!(:variant) { create(:variant, :sku => 'SPREE-T-SHIRT') }
-          let!(:stock_item) do
-            item = variant.stock_items.first
-            item.stock_location = stock_location
-            item.save
-            item
+          let(:variant) { create(:variant, :sku => 'SPREE-T-SHIRT') }
+
+          context "and the stock location name is equal to location in the message" do
+            let!(:stock_location) { create(:stock_location, name: 'us_warehouse')}
+
+            include_examples "updates inventory level and responds with proper message"
           end
 
-          it "will set the inventory to the supplied amount" do
-            expect{handler.process}.to change{stock_item.reload.count_on_hand}.from(0).to(93)
-          end
+          context "and the stock location admin name is equal to location in the message" do
+            let!(:stock_location) { create(:stock_location, name: 'a stock location name', admin_name: 'us_warehouse')}
 
-          it "returns a Hub::Responder with a proper message" do
-            responder = handler.process
-            expect(responder.summary).to eql "Set inventory for SPREE-T-SHIRT at us_warehouse from 0 to 93"
-            expect(responder.code).to eql 200
+            include_examples "updates inventory level and responds with proper message"
           end
         end
 
