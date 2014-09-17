7985ee4a4715dfaaea69e9292c964b3c0175d9fc
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-17 10:45:13 -0400

    Update wombat client to validate wombat response.

--- lib/spree/wombat/client.rb
+++ lib/spree/wombat/client.rb
@@ -10,13 +10,10 @@ module Spree
       def self.push_batches(object)
         object_count = 0
 
-        ts = Spree::Wombat::Config[:last_pushed_timestamps][object]
-        payload_builder = Spree::Wombat::Config[:payload_builder][object]
+        last_push_time = Spree::Wombat::Config[:last_pushed_timestamps][object] || Time.now
+        this_push_time = Time.now
 
-        unless ts
-          ts = Time.now
-          touch_last_pushed(object)
-        end
+        payload_builder = Spree::Wombat::Config[:payload_builder][object]
 
         scope = object.constantize
 
@@ -24,7 +21,7 @@ module Spree
           scope = scope.send(filter.to_sym)
         end
 
-        scope.where("updated_at > ?", ts).find_in_batches(batch_size: 10) do |batch|
+        scope.where(updated_at: last_push_time...this_push_time).find_in_batches(batch_size: 10) do |batch|
           object_count += batch.size
           payload = ActiveModel::ArraySerializer.new(
             batch,
@@ -33,33 +30,41 @@ module Spree
           ).to_json
 
           push(payload)
-          touch_last_pushed(object)
         end
+
+        update_last_pushed(object, this_push_time)
         object_count
       end
 
       def self.push(json_payload)
-        HTTParty.post(
-          Spree::Wombat::Config[:push_url],
-          {
-            body: json_payload,
-            headers: {
-             'Content-Type'       => 'application/json',
-             'X-Hub-Store'        => Spree::Wombat::Config[:connection_id],
-             'X-Hub-Access-Token' => Spree::Wombat::Config[:connection_token],
-             'X-Hub-Timestamp'    => Time.now.utc.to_i.to_s
-            }
-          }
-        )
+        res = HTTParty.post(
+                Spree::Wombat::Config[:push_url],
+                {
+                  body: json_payload,
+                  headers: {
+                   'Content-Type'       => 'application/json',
+                   'X-Hub-Store'        => Spree::Wombat::Config[:connection_id],
+                   'X-Hub-Access-Token' => Spree::Wombat::Config[:connection_token],
+                   'X-Hub-Timestamp'    => Time.now.utc.to_i.to_s
+                  }
+                }
+              )
+
+        validate(res)
       end
 
       private
-      def self.touch_last_pushed(object)
+      def self.update_last_pushed(object, new_last_pushed)
         last_pushed_ts = Spree::Wombat::Config[:last_pushed_timestamps]
-        last_pushed_ts[object] = Time.now
+        last_pushed_ts[object] = new_last_pushed
         Spree::Wombat::Config[:last_pushed_timestamps] = last_pushed_ts
       end
 
+      def self.validate(res)
+        raise PushApiError, "Push not successful. Wombat returned response code #{res.code} and message: #{res.body}" if res.code != 202
+      end
     end
   end
 end
+
+class PushApiError < StandardError; end
