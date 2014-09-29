cadc42c63030e25f062f11767a2da5fe48ab0435
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-29 19:51:39 -0400

    Fix broken spec.

--- lib/generators/spree_wombat/install/templates/wombat.rb
+++ lib/generators/spree_wombat/install/templates/wombat.rb
@@ -8,6 +8,7 @@ Spree::Wombat::Config.configure do |config|
   #   # By default we filter orders to only push if they are completed.  You can remove the filter to send incomplete orders as well.
   #   "Spree::Order" => { serializer: "Spree::Wombat::OrderSerializer", root: "orders", filter: "complete" },
   #   "Spree::Product" => { serializer: "Spree::Wombat::ProductSerializer", root: "products" },
+  #   "Spree::StockItem" => { serializer: "Spree::Wombat::StockItemSerializer", root: "inventories" }
   # }
   #config.push_url = "https://push.wombat.co"
 
