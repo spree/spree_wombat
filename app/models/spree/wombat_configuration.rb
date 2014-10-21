877ad7fd3ad262c84c882a23ed14c5f7c6a55658
Peter Berkenbosch <peter@pero-ict.nl>
2014-10-21 11:47:08 +0200

    not config "Cart" by default.

--- app/models/spree/wombat_configuration.rb
+++ app/models/spree/wombat_configuration.rb
@@ -4,16 +4,14 @@ module Spree
     preference :connection_id, :string
     preference :connection_token, :string
     preference :push_url, :string, :default => 'https://push.wombat.co'
-    preference :push_objects, :array, :default => ["Spree::Order", "Spree::Product", "Cart"]
+    preference :push_objects, :array, :default => ["Spree::Order", "Spree::Product"]
     preference :payload_builder, :hash, :default => {
       "Spree::Order" => {:serializer => "Spree::Wombat::OrderSerializer", :root => "orders", :filter => "complete", :model => "Spree::Order"},
       "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products", :model => "Spree::Product"},
-      "Cart" => {:serializer => "Spree::Wombat::OrderSerializer", :root => "carts", :filter => "cart", :model => "Spree::Order"}
     }
     preference :last_pushed_timestamps, :hash, :default => {
       "Spree::Order" => nil,
-      "Spree::Product" => nil,
-      "Cart" => nil
+      "Spree::Product" => nil
     }
   end
 end
