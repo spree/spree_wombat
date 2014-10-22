663ae67c3d95e6c937398d2615fe42ea421e00cc
Peter Berkenbosch <peter@pero-ict.nl>
2014-10-22 22:39:43 +0200

    inherit from basic product serializer to clean up

--- app/serializers/spree/wombat/jirafe/product_without_variants_serializer.rb
+++ app/serializers/spree/wombat/jirafe/product_without_variants_serializer.rb
@@ -4,54 +4,9 @@ module Spree
   module Wombat
     module Jirafe
 
-      class ProductWithoutVariantsSerializer < ActiveModel::Serializer
+      class ProductWithoutVariantsSerializer < Wombat::ProductSerializer
 
-        attributes :id, :name, :sku, :description, :price, :cost_price,
-                   :available_on, :permalink, :meta_description, :meta_keywords,
-                   :shipping_category, :options, :weight, :height, :width,
-                   :depth, :created_at, :updated_at, :taxons, :meta_data
-
-        has_many :images, serializer: Spree::Wombat::ImageSerializer
-
-        def created_at
-          object.created_at.getutc.try(:iso8601)
-        end
-
-        def updated_at
-          object.updated_at.getutc.try(:iso8601)
-        end
-
-        def id
-          object.sku
-        end
-
-        def price
-          object.price.to_f
-        end
-
-        def cost_price
-          object.cost_price.to_f
-        end
-
-        def available_on
-          object.available_on.try(:iso8601)
-        end
-
-        def permalink
-          object.slug
-        end
-
-        def shipping_category
-          object.shipping_category.name
-        end
-
-        def options
-          object.option_types.pluck(:name)
-        end
-
-        def taxons
-          object.taxons.collect {|t| t.self_and_ancestors.collect(&:name)}
-        end
+        attributes :meta_data
 
         def meta_data
           {
