b3b53307ef1ef8c2f2b367c19960f769a4282437
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-16 15:42:05 -0500

    If never pushed before start at beginning of time.

--- lib/spree/wombat/client.rb
+++ lib/spree/wombat/client.rb
@@ -10,7 +10,7 @@ module Spree
       def self.push_batches(object, ts_offset = 5)
         object_count = 0
 
-        last_push_time = Spree::Wombat::Config[:last_pushed_timestamps][object] || Time.now
+        last_push_time = Spree::Wombat::Config[:last_pushed_timestamps][object] || Time.at(0)
         this_push_time = Time.now
 
         payload_builder = Spree::Wombat::Config[:payload_builder][object]
